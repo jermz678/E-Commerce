8ff07f67239b1f6e06e4d4014434235beb4a5128
jermz678 <jermz678@live.com>
2021-05-31 15:26:08 -0500

    creating models

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,12 +1,22 @@
+const { dasherize } = require('inflection');
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const { truncate } = require('./Product.js');
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
